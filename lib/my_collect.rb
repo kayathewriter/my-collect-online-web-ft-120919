f0354d8d342eab2344bae9f28351955525cef290
Mikael Kaya <kayaswork@gmail.com>
2019-12-22 03:05:46 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,3 +1,22 @@
 require pry
 
-def my_collect
+def my_collect(array)
+  index = 0
+  modified_array = []
+  
+  
+  while index < array.length
+    modified_array << yield(array[index])
+    index += 1
+  end
+  
+  
+  modified_array
+end
+
+languages = ['ruby','javascript', 'python','objective-c']
+
+my_collect(languages) do |language|
+  binding.pry
+    language.upcase
+end
